4637766d6301145d618b269accf93939a317ae62
chaudharyrahulgcp <chaudharyrahulgcp@gmail.com>
2024-02-25 15:32:00 +0530

    some changes

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,14 @@
-terraform {
+/*terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = ">=4.67.0"
     }
   }
+  
+}*/
+provider "google" {
+  project = var.gcp_project_id
+  region = "us-central1"
+  
 }
