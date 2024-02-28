9f2c5137e7ddfa7fe8bbb3179f66062af8bd1a40
chaudharyrahulgcp <chaudharyrahulgcp@gmail.com>
2024-02-28 17:24:55 +0530

    some chan ges:

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,22 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks                   = true
   network_firewall_policy_enforcement_order = "BEFORE_CLASSIC_FIREWALL"
 }
+
+resource "google_compute_network" "gcp_network" {
+  project                                   = var.gcp_project_id
+  name                                      = "gcp-network"
+  auto_create_subnetworks                   = true
+  network_firewall_policy_enforcement_order = "BEFORE_CLASSIC_FIREWALL"
+}
+
+resource "google_compute_network_peering" "vpc_gcp_peering" {
+  name         = "vpc_gcp_peering"
+  network      = google_compute_network.vpc_network.self_link
+  peer_network = google_compute_network.gcp_network.self_link
+}
+
+resource "google_compute_network_peering" "gcp_vpc_peering" {
+  name         = "gcp_vpc_peering"
+  network      = google_compute_network.gcp_network.self_link
+  peer_network = google_compute_network.vpc_network.self_link
+}
