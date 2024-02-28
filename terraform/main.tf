723e931235e34264c2d1e2d01ede9a9f78aae0f6
chaudharyrahulgcp <chaudharyrahulgcp@gmail.com>
2024-02-28 17:15:50 +0530

    some chan ges:

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,11 @@ resource "google_storage_bucket" "sample" {
   project  = var.gcp_project_id
 
 }
+
+
+resource "google_compute_network" "vpc_network" {
+  project                                   = var.gcp_project_id
+  name                                      = "vpc-network"
+  auto_create_subnetworks                   = true
+  network_firewall_policy_enforcement_order = "BEFORE_CLASSIC_FIREWALL"
+}
